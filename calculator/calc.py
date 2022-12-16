31b50f32112d047ea47f8d64cb960d76f2971b68
Munchnax <Munchnax.jt@gmail.com>
2022-12-15 21:10:06 -0500

    Started Calculator Currently Non Functioning
    This is the first push... forgot to add a readme again unfortuantely...
    The goal of this calculator is first off to reaccustom myself to python,
    while also making myself more comfortable with posting my random things
    to the internet.
    
    This update provided:
    - A foundation of a calculator
    
    I need to get my mind back to using python for an upcoming competition..
    so good luck me!!! Hopefully I wont stare at the screen and wonder why
    my variables wont initialize (I kept using let fricking JavaScript...)
    Alright have a wonderful day, night, year?...!
    -Munchnax

---
+++ calculator/calc.py
@@ -0,0 +1,57 @@
+#making a calculator that can recive input and multiply divide or add and subtract
+
+empty_lst = []
+answer_lst = []
+m = False
+s= False
+d= False
+a= False
+
+print("This is a basic calculator made by Munchnax, if you would like to Multiply please type M\nIf you would like to Divide please type D",
+"If you would like to Subtract please type S\n If you would like to Add please type A")
+
+
+selection = input("Your selection: ")
+result = 1
+match selection:
+    case "M":
+        usernumbers = input("Please enter atleast two numbers") 
+        for i in usernumbers:
+            empty_lst.append()
+            m = True
+    case "m":
+        usernumbers = input("Please enter atleast two numbers") 
+        for i in usernumbers:
+            empty_lst.append(i)
+    case "D":
+            usernumbers = input("Please enter atleast two numbers") 
+            for i in usernumbers:
+                empty_lst.append(i)
+                d = True
+    case "S":
+            usernumbers = input("Please enter two atleast numbers")
+            for i in usernumbers:
+                empty_lst.append(i)
+                s = True
+    case "A":
+            usernumbers = input("Please enter two atleast numbers") 
+            for i in usernumbers:
+                empty_lst.append(i)
+                a = True
+    case _:
+        print("Well please try again later")
+
+for item in empty_lst:
+    item = float(item)
+    if m == True or d == True:
+        result = 1
+        if m == True: result = item * result
+        else: result = item / result
+    if s == True or a == True:
+        result = 0
+        if a == True: result = item + result
+        else: result = result - item
+
+answer_lst.append(result)
+
+print(f"Result: {answer_lst}")
