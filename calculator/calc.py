2c9b3565cff670f5a0bf59b7872937dc3e4c6ca8
Munchnax <Munchnax.jt@gmail.com>
2022-12-16 19:50:10 -0500

    Fully Working Calculator
    Finally got it working will be my last push untill I understand how to
    use Python with graphics, or find time to lock it down and prevent bugs.
    
    This update:
        -Rebuilt the program with proper functions
        -Cleaned up past committed code
        -Added comments to explain to future me/others
    
    As normal thanks for reading this and messing around with my code, find
    anything I can improve/resources you'd think might help me hit me up my
    email is munchnax.jt@gmail.com.
    -Munchnax

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,66 +1,55 @@
-#making a calculator that can recive input and multiply divide or add and subtract
+# This program calculates based on user input and selected numbers
 
 def multiply(numbers):
     math = 1
     for number in numbers:
         number = int(number)
         math = number * math
-    return math
-print(multiply(input("Type in numbers: ")))
+    return f"The answer is: {math}"
+
+def division(numbers):
+    math = 1
+    for number in numbers:
+        number = int(number)
+        math = number / math
+    return f"The answer is: {math}"
+
+def subtraction(numbers):
+    math = 0
+    i = 0
+    for number in numbers:
+        number = int(number)
+        # if i not grater than 1 do number - math this is there to prevent the answer from being a negative number
+        if i <= 0:
+            math = number - math
+            i = 1
+        else: math = math - number
+            
+    return f"The answer is: {math}"
+
+def addition(numbers):
+    math = 0
+    for number in numbers:
+        number = int(number)
+        math = number + math
+    return f"The answer is: {math}"
 
 
-# empty_lst = []
-# answer_lst = []
-# m = False
-# s= False
-# d= False
-# a= False
+print("This is a calculator built by Munchnax! This update was made on Dec 16, 2022... Hopefully it works.... ")
+print("If you would like to Multiply Please type M:\nIf you would like to Divide Please type D:\nIf you would like to Subtract Please type S:\nIf you would like to do Addition Please type A:\n")
 
-# print("This is a basic calculator made by Munchnax, if you would like to Multiply please type M\nIf you would like to Divide please type D",
-# "If you would like to Subtract please type S\n If you would like to Add please type A")
+selection = input("Your Response: ")
 
+match selection:
+    case "m":
+        print(multiply(input("Type in numbers: ").split()))        
+    case "d":
+        print(division(input("Type in numbers: ").split()))
+    case "a":
+        print(addition(input("Type in numbers: ").split()))
+    case "s":
+        print(subtraction(input("Type in numbers: ").split()))
+    case _:
+        print("Welp, either my code isn't working or you broke it... Goodjob?..!?")
 
-# selection = input("Your selection: ")
-# result = 1
-# match selection:
-#     case "M":
-#         usernumbers = input("Please enter atleast two numbers") 
-#         for i in usernumbers:
-#             empty_lst.append()
-#             m = True
-#     case "m":
-#         usernumbers = input("Please enter atleast two numbers") 
-#         for i in usernumbers:
-#             empty_lst.append(i)
-#     case "D":
-#             usernumbers = input("Please enter atleast two numbers") 
-#             for i in usernumbers:
-#                 empty_lst.append(i)
-#                 d = True
-#     case "S":
-#             usernumbers = input("Please enter two atleast numbers")
-#             for i in usernumbers:
-#                 empty_lst.append(i)
-#                 s = True
-#     case "A":
-#             usernumbers = input("Please enter two atleast numbers") 
-#             for i in usernumbers:
-#                 empty_lst.append(i)
-#                 a = True
-#     case _:
-#         print("Well please try again later")
-
-# for item in empty_lst:
-#     item = float(item)
-#     if m == True or d == True:
-#         result = 1
-#         if m == True: result = item * result
-#         else: result = item / result
-#     if s == True or a == True:
-#         result = 0
-#         if a == True: result = item + result
-#         else: result = result - item
-
-# answer_lst.append(result)
-
-# print(f"Result: {answer_lst}")
+print("Thanks for testing the program, any issues please email me munchnax.jt@gmail.com or just mssg me on github!")
